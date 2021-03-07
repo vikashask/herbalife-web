caf6715ae5dc3ecce8d634fd9c02d1e06b82e526
vikash <vikash.verma@pepsico.com>
2021-03-07 14:01:51 +0530

    MainLandingPage commented

--- src/App.js
+++ src/App.js
@@ -102,7 +102,7 @@ import RestaurantLandingPage from "demos/RestaurantLandingPage.js";
 // import PrivacyPolicyPage from "pages/PrivacyPolicy.js";
 
 import ComponentRenderer from "ComponentRenderer.js";
-import MainLandingPage from "MainLandingPage.js";
+// import MainLandingPage from "MainLandingPage.js";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
